f6a0444ef4ee051efcab1d72c7cde78ebc903a70
PrasannaMantha <prasana.mantha@gmail.com>
2015-10-19 11:41:58 +0530

    my first commit

---
+++ Sum.java
@@ -0,0 +1,12 @@
+class Sum
+{
+public static void main(String args[])
+{
+int num;
+num=10;
+System.out.println("This is num:" + num);
+num = num * 2;
+System.out.println("The value of num * 2 is");
+System.out.println(num);
+}
+}
