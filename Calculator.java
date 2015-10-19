333502dff54e01f079f5e01189754830f3b22dd3
PrasannaMantha <prasana.mantha@gmail.com>
2015-10-19 11:59:26 +0530

    my first commit

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+class Calculator
+{
+int z;
+public void Addition(int x, int y)
+{
+z=x+y;
+System.out.println("the sum of the given numbers:"+z);
+}
+public void Subtraction(int x,int y)
+{
+z=x-y;
+System.out.println("the difference between the given numbers:"+z);
+}
+}
