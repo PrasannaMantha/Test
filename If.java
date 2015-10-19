6f52fa6334c3338fd8e7454ab8011feb372808ce
PrasannaMantha <prasana.mantha@gmail.com>
2015-10-19 12:25:17 +0530

    my first commit

---
+++ If.java
@@ -0,0 +1,17 @@
+class If
+{
+public static void main(String args[])
+{
+int x,y;
+x=10;
+y=30;
+if(x < y)
+System.out.println("x is less than y");
+x = x * 2;
+if(x == y)
+System.out.println("x is now equal to y");
+x = x * 2;
+if(x > y)
+System.out.println("x is now greater than y");
+}
+}
